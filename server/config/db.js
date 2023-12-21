cd4bb4a413c490a384201ce3bccf11daf0e3a3f9
TheBeast <ozajatin9309@gmail.com>
2023-12-21 15:01:30 +0530

    backend setup done

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ import "dotenv/config.js";
 
 const connectDB = async () => {
 	try {
-		console.log(process.env.MONGO_URI);
+		// console.log(process.env.MONGO_URI);
 		const conn = await mongoose.connect(process.env.MONGO_URI, {
 			useUnifiedTopology: true,
 			useNewUrlParser: true,
